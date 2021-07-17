296e73110aae87f20935850794f91409cfadf224
SeongnamFC <seongnamfc@mac-sldt-chaeyoungjun.local>
2021-07-17 21:30:13 +0900

    Initial commit

---
+++ first-step.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Syntax</title>
+</head>
+
+<body>
+Test !!
+i have launched PHP but.... how can i show up ??
+<?php
+echo "PHP 수업에 잘 오셨습니다!";
+?>
+
+</body>
+
+</html>
