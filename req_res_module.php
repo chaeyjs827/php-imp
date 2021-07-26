cd9b60e052dfc2800d63a208937b7e5bfd8afcf6
seongnamfc <chaeyjs827@gmail.com>
2021-07-26 20:49:36 +0900

    php 어려워 ㅠ_ㅠ

---
+++ req_res_module.php
@@ -0,0 +1,18 @@
+<?
+
+/*
+ * 7월 27일 data parsing 작업은 이부분에서 실행해야 함.
+ */
+class TodayTest{
+
+    public $reqeuset_param = null;
+
+    function test($params) {
+        echo 'hello guys !';
+    }
+
+}
+
+$test_obj = new TodayTest();
+
+$test_obj->test();
