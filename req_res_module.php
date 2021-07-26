3c3ca00e0d2a0fdff26d54acfb9d54b18e97a3ee
seongnamfc <chaeyjs827@gmail.com>
2021-07-26 23:37:16 +0900

    php 어려워 ㅠ_ㅠ

--- req_res_module.php
+++ req_res_module.php
@@ -5,14 +5,32 @@
  */
 class TodayTest{
 
+    const SITE_KEY = 'SOLDOUT';
+    const SITE_SECRET = '282618ce';
+    const SITE_AES_KEY = 'KZJKDCTO9835fzcejmic5688WBSADCFB';
+
+    private $defaultReqType;
+    private $baseApiUrl;
+    private $baseWebUrl;
+    private $defaultHeader;
+
+    private $aesIv;
+    private $aesMethod;
+
+
     public $reqeuset_param = null;
 
-    function test($params) {
-        echo 'hello guys !';
+    function getInfo($reqType, $reqUrl, $header) {
+//        $this_method_name = method_get_name();
+        echo $reqType . $reqUrl . $header;
+        $method_name = __FUNCTION__;
+        echo $method_name;
     }
 
 }
 
 $test_obj = new TodayTest();
 
-$test_obj->test();
+//$test_obj->test('asdasdasdasd');
+
+$test_obj->getInfo('hahah','hohoho','heghehe       ss');
