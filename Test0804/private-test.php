2aabf2cc25637a77306d5087a6cf2547edb35d1c
seongnamfc <chaeyjs827@gmail.com>
2021-08-04 22:46:27 +0900

    `php 어려워 ㅠ_ㅠ`

--- Test0804/private-test.php
+++ Test0804/private-test.php
@@ -6,8 +6,12 @@ class TestLib {
     private $private_value;
     public $public_value;
 
-    public function __contruct($pri_v, $pub_v) {
+    public function __construct($pri_v, $pub_v) {
         $this->private_value = $pri_v;
         $this->public_value = $pub_v;
+    }
+
+    public function getPrivateValue() {
+        return $this->private_value;
     }
 }
--- Test0804/pt-main.php
+++ Test0804/pt-main.php
@@ -14,4 +14,11 @@ class TestLibMain {
 
 $a = TestLibMain::getObj();
 echo $a->public_value;
+echo json_encode($a);
 
+$b = new TestLib("haha", "hoho");
+$b->public_value = 100;
+echo $b->public_value."</br>";
+echo json_encode($b);
+
+echo $b->getPrivateValue();
