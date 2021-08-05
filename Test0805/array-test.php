6a2ec984f94d42f2ad728f3d9d237e4bd70c270b
seongnamfc <chaeyjs827@gmail.com>
2021-08-05 20:56:22 +0900

    `php 어려워 ㅠ_ㅠ`

---
+++ Test0805/array-test.php
@@ -0,0 +1,66 @@
+<?php
+
+class ArrayEncodingTest {
+
+    public static function test() {
+        $testArray = [
+            'aa' => 'bb',
+            'ab' => 'bb',
+            'ac' => 'bb',
+            'ad' => 'bb',
+            'ae' => 'bb',
+            'af' => 'bb',
+            'ag' => 'bb',
+            'ah' => 'bb',
+            'ai' => 'bb',
+            'aj' => 'bb',
+            'ak' => 'bb',
+            'al' => 'bb',
+            'am' => 'bb',
+            'an' => 'bb',
+            'ao' => 'bb',
+            'ap' => 'bb',
+            'ao' => 'bb',
+            'ap' => 'bb',
+            'aq' => 'bb',
+            'ar' => 'bb',
+            'as' => 'bb',
+            'at' => 'bb',
+            'au' => 'bb',
+            'aw' => 'bb',
+            'ax' => 'bb',
+            'ay' => 'bb',
+            'az' => 'bb',
+            'aca' => 'bb',
+            'ada' => 'bb',
+            'aea' => 'bb',
+            'afa' => 'bb',
+            'aga' => 'bb',
+            'aha' => 'bb',
+            'aia' => 'bb',
+            'aja' => 'bb',
+            'aka' => 'bb',
+            'ala' => 'bb',
+            'ama' => 'bb',
+            'ana' => 'bb',
+            'aoa' => 'bb',
+            'apa' => 'bb',
+            'aoa' => 'bb',
+            'apa' => 'bb',
+            'aqa' => 'bb',
+            'ara' => 'bb',
+            'asa' => 'bb',
+            'ata' => 'bb',
+            'aua' => 'bb',
+            'awa' => 'bb',
+            'axa' => 'bb',
+            'aya' => 'bb',
+            'aza' => 'bb',
+            'aaaa' => 'bb'
+            ];
+        return json_encode($testArray);
+    }
+
+}
+
+echo ArrayEncodingTest::test();
