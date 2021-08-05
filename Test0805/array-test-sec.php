48128803db541b9ac88afab4112df1d08d796dc6
seongnamfc <chaeyjs827@gmail.com>
2021-08-05 20:57:48 +0900

    `php 어려워 ㅠ_ㅠ`

---
+++ Test0805/array-test-sec.php
@@ -0,0 +1,18 @@
+<?php
+
+class ArrayEncodingTest {
+
+    public static function test() {
+        $testArray = [
+            'aa' => 'bb'
+        ];
+
+        $testArraySec = [
+            'cc' => 'rr'
+        ];
+        return json_encode([$testArray, $testArraySec]);
+    }
+
+}
+
+echo ArrayEncodingTest::test();
