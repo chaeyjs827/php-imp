0684d6757557620c5f56de8709947d1d056b0428
seongnamfc <chaeyjs827@gmail.com>
2021-08-06 21:25:34 +0900

    `php 어려워 ㅠ_ㅠ`

--- Test0806/enc-test.php
+++ Test0806/enc-test.php
@@ -5,9 +5,12 @@ class ArrayEncodingTest {
     public static function test() {
 //        $str = 'this is utf-8';         // 이게 왜 ascii지...?
 //        $str = 'res_msg=����ó��&';  // UTF-8
-        $str = 'CKM&acqu_name=KB����ī��, 0';
+//        $str = '��-��m';    // Base64
+        $str = 'hahahahahaha';  // euc-kr
 
-        $encode = array('ASCII', 'UTF-8', 'EUC-KR');
+//        $str = 'CKM&acqu_name=KB����ī��, 0';
+
+        $encode = array('ASCII', 'UTF-8', 'EUC-KR', 'Base64');
         $str_encode = mb_detect_encoding($str, $encode);
 
 //        echo '<br>'.mb_detect_encoding($str, 'EUC-KR, UTF-8');
@@ -15,8 +18,17 @@ class ArrayEncodingTest {
 //        echo '<br>'.mb_detect_encoding($str, array('EUC-KR','UTF-8'));
 
         echo '<br>'.$str_encode;
+    }
 
+    public static function encToEnc() {
+        $str = 'test test';
+        echo '<br>'.iconv('EUC-KR', 'UTF-8', $str);
+        echo '<br>'.iconv('UTF-8', 'EUC-KR', $str);
+//        $saveData['req_body'] = json_encode([
+//            iconv("EUC-KR", "UTF-8",$payPlus->getModxData())
+//        ]);
     }
 }
 
 echo ArrayEncodingTest::test();
+echo ArrayEncodingTest::encToEnc();
