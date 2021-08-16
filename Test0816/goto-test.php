e6cda56a6b073594839ff658fc056baa9784eb2d
seongnamfc <chaeyjs827@gmail.com>
2021-08-16 22:44:51 +0900

    `php 어려워 ㅠ_ㅠ`

---
+++ Test0816/goto-test.php
@@ -0,0 +1,27 @@
+<?php
+
+echo '1<br>';
+echo '2<br>';
+echo '3<br>';
+echo '4<br>';
+goto gotoLabelTest;
+echo '5<br>';
+echo '6<br>';
+echo '7<br>';
+echo '8<br>';
+gotoLabelTest:
+echo '9<br>';
+echo '10<br>';
+
+
+echo "everdevel step 1 <br>";
+echo "everdevel step 2 <br>";
+goto go7;
+echo "everdevel step 3 <br>";
+echo "everdevel step 4 <br>";
+echo "everdevel step 5 <br>";
+echo "everdevel step 6 <br>";
+go7:
+echo "everdevel step 7 <br>";
+echo "everdevel step 8 <br>";
+echo "everdevel step 9 <br>";
