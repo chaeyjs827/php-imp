53c660a503abb8db97780e2335ac168d4becfad6
seongnamfc <chaeyjs827@gmail.com>
2021-08-21 18:04:48 +0900

    `php 어려워 ㅠ_ㅠ`

---
+++ Test0821/const-test.php
@@ -0,0 +1,11 @@
+<?php
+
+const MIN_NUM = 0;
+const MAX_NUM = 100;
+
+echo MAX_NUM.'<br>';
+echo MIN_NUM.'<br>';
+
+//MIN_NUM = 1000; // 재정의는 가능 하지만 값을 변경 할 순 없음
+const MIN_NUM = 1;
+echo MIN_NUM.'<br>';
