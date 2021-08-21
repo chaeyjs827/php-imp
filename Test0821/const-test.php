43f4bb36d7fd2f44ba33ab103d04ceeef86cec5a
seongnamfc <chaeyjs827@gmail.com>
2021-08-21 18:17:23 +0900

    `php 어려워 ㅠ_ㅠ`

--- Test0821/const-test.php
+++ Test0821/const-test.php
@@ -9,3 +9,20 @@ echo MIN_NUM.'<br>';
 //MIN_NUM = 1000; // 재정의는 가능 하지만 값을 변경 할 순 없음
 const MIN_NUM = 1;
 echo MIN_NUM.'<br>';
+
+function test() {
+    echo 'test : '.MIN_NUM.'<br>';
+}
+
+test();
+
+class ConstTest {
+    const HUNGRY = 100;
+
+    function test() {
+        echo 'ConstTest : '.self::HUNGRY.'<br>';
+    }
+}
+
+$t = new ConstTest();
+$t->test();
