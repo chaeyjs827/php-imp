1083e8c664bb8dd8b154a186a1ef719f2d0438e2
seongnamfc <chaeyjs827@gmail.com>
2021-08-29 23:50:26 +0900

    `php 어려워 ㅠ_ㅠ`

---
+++ Test0829/const-test.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Libraries\Log\Logger;
+
+class ConstIfTest {
+
+     const COMPANY_M = 'm';
+     const COMPANY_S ='s';
+     const HOLIDAY_API = 'h';
+
+    public static function mainMethod($apiSubjectName) {
+        echo self::getResult($apiSubjectName);
+    }
+
+    public static function getResult($apiSubjectName) {
+        if($apiSubjectName == self::COMPANY_M) {
+            echo 'com m';
+        } else if($apiSubjectName == self::HOLIDAY_API) {
+            echo 'com h';
+        } else if($apiSubjectName == self::COMPANY_S) {
+            echo 'com s';
+        }
+    }
+
+
+}
+
+ConstIfTest::mainMethod('h');
