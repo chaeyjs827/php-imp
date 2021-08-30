cb9a5645777607af102da8bdbe379a47f0d266ca
seongnamfc <chaeyjs827@gmail.com>
2021-08-30 22:59:29 +0900

    `php 어려워 ㅠ_ㅠ`

--- Test0830/request-test.php
+++ Test0830/request-test.php
@@ -13,14 +13,20 @@ class RequestTest {
         $curl = curl_init();
 
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 3);
+//        curl_setopt($curl, CURLOPT_TIMEOUT, 1);
 
         $result = curl_exec($curl);
 
-        return json_encode($result);
-
+        if($result == false) {
+            echo 'time-out error발생인디';
+        } else {
+            return json_encode($result);
+        }
     }
 
 
 }
 
+//echo RequestTest::mainMethod('common-rest.sweettracker.net/tracking?t_key=o1SqrQ1rTDPNcHeGW1pslg&t_code=CJGLS&t_invoice=111111111111');
 echo RequestTest::mainMethod('https://jsonplaceholder.typicode.com/posts');
